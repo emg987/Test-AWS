571c22f299cffa38a9d844f1f0df689c43a1291e
emg987 <emg987@gmail.com>
2014-12-03 09:16:43 -0800

    SetNTP.ps1 PowerShell script
    Committing #1 PowerShell script

---
+++ SetNTP.ps1
@@ -0,0 +1,13 @@
+﻿
+
+# PDC emulator operations master role
+$forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest();
+Write-Host "Forest Name:" $trace.Name;
+$forest.RootDomain.PdcRoleOwner.Name;
+
+# Link the domain controller to an external time source
+w32tm.exe /config /manualpeerlist:"0.us.pool.ntp.org 1.us.pool.ntp.org 2.us.pool.ntp.org 3.us.pool.ntp.org" /syncfromflags:manual /reliable:YES /update;
+w32tm.exe /config /update;
+
+# Restart the Windows Team service
+Restart-Service w32time;
